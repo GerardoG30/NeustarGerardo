f6c2f95e91663d6f4dd332e5a54af346738ee1fc
Jose Gerardo González Muñoz <jggonzalez@pelpago.com>
2020-07-12 12:25:48 -0600

    Agregar archivos de proyecto.

---
+++ Technical_Test_Gerardo/SortingSquare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Technical_Test_Gerardo
+{
+    class SortingSquare
+    {
+        static void Main(string[] args)
+        {
+
+            var arrayAscending = SortingSquares(new int[] { -1, 0, 2 });
+            string array = string.Join(",", arrayAscending);
+            Console.Write(array);
+            Console.ReadLine();
+        }
+
+
+
+        public static int[] SortingSquares(int[] arrayNumbers)
+        {
+            List<int> arrayNumbersFinal = new List<int>();
+            foreach (var num in arrayNumbers)
+            {
+                var square = Convert.ToInt32(Math.Pow(num, 2));
+                arrayNumbersFinal.Add(square);
+            }
+            return arrayNumbersFinal.OrderBy(number => number).ToArray();
+        }
+    }
+}
